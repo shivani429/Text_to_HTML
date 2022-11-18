c3455a1b3fb890d2b5248284dfb5a197a29171b3
shivani <113819244+shivani429@users.noreply.github.com>
2022-11-18 15:59:18 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -3,36 +3,4 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Blog)
-
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'Roll_no', 'class_no', 'Address')
-
-@admin.register(Employee)
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'eid', 'ename' ,'ephn', 'esalary')
-
-admin.site.register(EmployeeSalary)
-
-admin.site.register(Details)
-
 admin.site.register(Editor)
-
-admin.site.register(Demoapp)
-
-admin.site.register(Profile)
-
-admin.site.register(Photo)
-
-admin.site.register(Contact)
-
-admin.site.register(QuizModel)
-
-@admin.register(CrudUser)
-class CrudUserAdmin(admin.ModelAdmin):
-    list_display = ('name', 'address', 'age')
-
-admin.site.register(Emp)
-
-
